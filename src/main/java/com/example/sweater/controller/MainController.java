b277d9f4679dd4e13cf8b5500fe7711665bbbf08
Смирнов Роман Валентинович <RVaSmirnov@sberbank.ru>
2021-01-29 09:28:34 +0300

    lesson 3

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -1,14 +1,11 @@
-package com.example.sweater;
+package com.example.sweater.controller;
 
 import com.example.sweater.domain.Message;
-import com.example.sweater.domain.repos.MessageRepo;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.sweater.repos.MessageRepo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -16,27 +13,27 @@ import java.util.Map;
  * @author Roman
  */
 @Controller
-public class GreetingController {
-    @Autowired
-    private MessageRepo messageRepo;
+public class MainController {
+    private final MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(
-            @RequestParam(name = "name", required = false, defaultValue = "World") String name,
-            Map<String, Object> model
-    ) {
-        model.put("name", name);
+    public MainController(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
+
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
+//        model.put("name", name);
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     public String add(@RequestParam String text,
                       String tag,
                       Map<String, Object> model) {
