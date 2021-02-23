5b8f45d18162343a05b2f72dccaa6428a08bc4a6
Смирнов Роман Валентинович <RVaSmirnov@sberbank.ru>
2021-02-23 15:53:48 +0300

    lesson 8

--- src/main/java/com/example/sweater/domain/User.java
+++ src/main/java/com/example/sweater/domain/User.java
@@ -22,6 +22,26 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getActivationCode() {
+        return activationCode;
+    }
+
+    public void setActivationCode(String activationCode) {
+        this.activationCode = activationCode;
+    }
+
+    private String activationCode;
+
     public Long getId() {
         return id;
     }
@@ -32,6 +52,10 @@ public class User implements UserDetails {
 
     public String getUsername() {
         return username;
+    }
+
+    public boolean isAdmin() {
+        return roles.contains(Role.ADMIN);
     }
 
     @Override
